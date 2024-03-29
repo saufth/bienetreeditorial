c20ab5be466cbda4d363f05c5c645a38ceed9cf4
saufth <saufth@gmail.com>
2024-03-29 09:49:23 -0600

    Refactor mode toggle icons

--- src/components/layouts/mode-toggle.tsx
+++ src/components/layouts/mode-toggle.tsx
@@ -11,11 +11,11 @@ export function ModeToggle () {
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <Icons.Sun
-        className='h-auto w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
+        className='h-auto w-6 rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0'
         aria-hidden='true'
       />
       <Icons.Moon
-        className='absolute top-0 h-auto w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
+        className='absolute top-0 h-auto w-6 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100'
         aria-hidden='true'
       />
       <span className='sr-only'>Toggle theme</span>
