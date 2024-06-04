ad831b8455a3dc012bc4c464c6144410cce8fd04
saufth <saufth@gmail.com>
2024-06-04 00:50:25 -0600

    refatoring structure: headline-test

--- src/components/headline.tsx
+++ src/components/headline.tsx
@@ -1,12 +1,12 @@
 import { Highlight } from '@/components/highlight'
 
 export const Headline = ({ children }: { children: string }) => {
-  const headlineData = children.split(' ')
+  const headline = children.split(' ')
 
   return (
     <h1 className='f-display-1 font-header x:leading-[150%] sm:leading-[140%] xl:leading-[140%] -tracking-tight text-right'>
-      {headlineData.map((word, key) => {
-        return key < (headlineData.length - 1)
+      {headline.map((word, key) => {
+        return key < (headline.length - 1)
           ? `${word} `
           : (
             <span className='w-full block relative z-0' key={key}>
